130c00d6ddffd514f9d4820748368053e38bc9a0
zhen <zhen@MacBook-Air-Katsiaryna.local>
2018-03-17 16:04:09 +0200

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@
                       label="Name"
                       v-model="name"
                       :rules="nameRules"
-                      :counter="10"
+                      :counter="100"
                       required
                     ></v-text-field>
                     <v-text-field
@@ -47,6 +47,21 @@
 </template>
 
 <script>
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+
+// Import our contract artifacts and turn them into usable abstractions.
+import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+
+// MetaCoin is our usable abstraction, which we'll use through the code below.
+var MetaCoin = contract(metacoin_artifacts);
+// The following code is simple to show off interacting with your contracts.
+// As your needs grow you will likely need to change its form and structure.
+// For application bootstßrapping, check out window.addEventListener below.
+var accounts;
+var account;
+
 export default {
   name: "HelloWorld",
   props: {
@@ -58,7 +73,7 @@ export default {
       name: "",
       nameRules: [
         v => !!v || "Name is required",
-        v => v.length <= 10 || "Name must be less than 10 characters"
+        v => v.length <= 100 || "Name must be less than 100 characters"
       ],
       docnumber:"",
       docnumberRules: [
@@ -81,7 +96,35 @@ export default {
   }, 
   methods: {
       submit () {
-        if (this.$refs.form.validate()) {
+
+        if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+  }
+    //    if (this.$refs.form.validate()) {
+           // Bootstrap the MetaCoin abstraction for Use.
+    MetaCoin.setProvider(web3.currentProvider);
+
+    // Get the initial account balance so it can be displayed.
+    web3.eth.getAccounts(function(err, accs) {
+      if (err != null) {
+        alert("There was an error fetching your accounts.");
+        return;
+      }
+
+      if (accs.length == 0) {
+        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+        return;
+      }
+
+      accounts = accs;
+      account = accounts[0];
+    });
           // Native form submission is not yet supported
           axios.post('/api/submit', {
             name: this.name,
@@ -94,7 +137,7 @@ export default {
       clear () {
         this.$refs.form.reset()
       }
-    }
+   // }
 
 };
 </script>
