920da4bc8aca4918b1bcd87f1ee89a86784c9040
zhen <zhen@MacBook-Air-Katsiaryna.local>
2018-03-17 15:21:05 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,81 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Contract Smarter</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+           <!-- <v-tooltip left>
+              <v-btn icon large :href="source" target="_blank" slot="activator">
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>-->
+              <HelloWorld msg="Welcome to App"/>
+ 
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</div>
+
+ 
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+
+Vue.use(Vuetify)
+
 
 export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+
+  data: () => ({
+    drawer: null,
+    source:'/'
+  })
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,102 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-layout>
+    <v-flex xs24 sm12  justify-center  align-center  >
+      <v-card style="min-height:300px; max-width:500px; margin-left:auto; margin-right:auto;">
+        <v-card-title primary-title>
+          <div style="width:100%;">
+            <h3  style="margin-top:1rem;"  class="headline">Baller Tournament</h3>
+             <v-form v-model="valid">
+                    <v-text-field
+                      label="Name"
+                      v-model="name"
+                      :rules="nameRules"
+                      :counter="10"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      label="E-mail"
+                      v-model="email"
+                      :rules="emailRules"
+                      required
+                    ></v-text-field>
+                     <v-text-field
+                      label="Document number"
+                      v-model="docnumber"
+                      :rules="docnumberRules"
+                      :counter="10"
+                      required
+                    ></v-text-field>
+
+                     <v-checkbox 
+                          label="I agree to pay for the participation"
+                          v-model="checkbox"
+                          :rules="[v => !!v || 'You must agree to continue!']"
+                          required
+                        ></v-checkbox>
+
+                          <v-btn  color="success" @click="submit"  :disabled="!valid"  >Pay and Register </v-btn>           
+                  </v-form>
+          </div>
+        </v-card-title>
+      <!--  <v-card-actions>
+         <v-btn @click="submit"  :disabled="!valid"  >Pay and Register </v-btn>
+        </v-card-actions>-->
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
-  }
-}
+  },
+  data() {
+    return {
+      valid: false,
+      name: "",
+      nameRules: [
+        v => !!v || "Name is required",
+        v => v.length <= 10 || "Name must be less than 10 characters"
+      ],
+      docnumber:"",
+      docnumberRules: [
+        v => !!v || "Docnumber is required",
+        v => v.length > 10 || "Docnumber must be more than 10 characters"
+      ],
+
+      email: "",
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v =>
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          "E-mail must be valid"
+      ],  
+      checkbox: false,
+      
+      card_text:
+        "Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat."
+    };
+  }, 
+  methods: {
+      submit () {
+        if (this.$refs.form.validate()) {
+          // Native form submission is not yet supported
+          axios.post('/api/submit', {
+            name: this.name,
+            email: this.email,
+            select: this.select,
+            checkbox: this.checkbox
+          })
+        }
+      },
+      clear () {
+        this.$refs.form.reset()
+      }
+    }
+
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
